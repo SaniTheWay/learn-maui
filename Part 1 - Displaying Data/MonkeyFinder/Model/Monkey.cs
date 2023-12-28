b93f14086776f99707928c229d18d5a27e48bdc3
Sanidhya Dave <sanidhyad16@gmail.com>
2023-12-28 15:32:40 +0530

    basicLayout

--- Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Model/Monkey.cs
@@ -4,4 +4,11 @@ namespace MonkeyFinder.Model;
 
 public class Monkey
 {
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Detail { get; set; }
+    public string Image { get; set; }
+    public string Population { get; set; }
+    public string Latitude { get; set; }
+    public string Longitude { get; set; }
 }
